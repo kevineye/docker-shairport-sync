d22ad618d19e5a037dea6ed094970818365c7b63
Kevin Eye <eye@buffalo.edu>
2017-07-23 07:27:03 -0400

    fixed pid issue in start script

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+rm -rf /var/run
 mkdir -p /var/run/dbus
-dbus-daemon --system && avahi-daemon -D && shairport-sync -m avahi -a "$AIRPLAY_NAME" "$@"
+
+dbus-uuidgen --ensure
+dbus-daemon --system
+
+avahi-daemon --daemonize --no-chroot
+
+shairport-sync -m avahi -a "$AIRPLAY_NAME" "$@"
